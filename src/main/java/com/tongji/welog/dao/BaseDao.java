69e6a4897636df1ac47339f254c949e513f70cf6
ChaunceyJ <33971563+ChaunceyJ@users.noreply.github.com>
2020-06-22 15:51:00 +0800

    add readme

--- src/main/java/com/tongji/welog/dao/BaseDao.java
+++ src/main/java/com/tongji/welog/dao/BaseDao.java
@@ -1,14 +1,12 @@
 package com.tongji.welog.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class BaseDao {
+public class BaseDao{
     static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    static final String DB_URL = "jdbc:oracle:thin:@100.68.136.238:1521:orcl";
-    static final String USER = "chend";
-    static final String PASS = "root";
+    static final String DB_URL = "jdbc:oracle:thin:@100.24.61.19:1521:orcl";
+    static final String USER = "jiang";
+    static final String PASS = "1234";
 
     Connection getConn() throws ClassNotFoundException, SQLException {
         Connection conn = null;
@@ -16,5 +14,36 @@ public class BaseDao {
         conn = DriverManager.getConnection(DB_URL, USER, PASS);
         System.out.println("Connected to the database.");
         return conn;
+    }
+
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement sql = conn.createStatement();
+            long start1 =  System.currentTimeMillis();
+            sql.execute("INSERT INTO POST(POST_ID, POST.CONTENT, POST.USER_ID, POST.TIME, POST.DELETE_FLAG) VALUES (999,'dfqrqr',1,0)");
+            long end1 =  System.currentTimeMillis();
+            double used1 = (end1-start1)/1000;
+            PreparedStatement psql = conn.prepareStatement("INSERT INTO POST(POST_ID, POST.CONTENT, POST.USER_ID, POST.TIME, POST.DELETE_FLAG) VALUES (?,?,?,?)");
+            psql.setInt(1, 1000);
+            psql.setInt(2, 1);
+            psql.setString(3, "wqreqwwad");
+            psql.setInt(4,0);
+            long start2 =  System.currentTimeMillis();
+            psql.execute();
+            long end2 =  System.currentTimeMillis();
+            double used2 = (end2-start2)/1000;
+            System.out.println(used1);
+            System.out.println(used2);
+            psql.close();
+            sql.close();
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
